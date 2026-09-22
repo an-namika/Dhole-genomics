fb82b81ec88791d395095fe344c9581797dffea5
Annamika Konkola <ak308@rice.edu>
2026-09-22 15:40:00 -0500

    Update fastqArray_COMPLETE.sh

--- 00_alignment/fastqArray_COMPLETE.sh
+++ 00_alignment/fastqArray_COMPLETE.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --time=14:00:00
 #SBATCH --mem=1G
-#SBATCH --array=1-62%10                  # 62 active files, max 6 downloading at once (suggested to not overload system?)
+#SBATCH --array=1-62%6               
 #SBATCH --output=/home/ak308/Dholes/output/fastq_dl_%A_%a.out
 #SBATCH --error=/home/ak308/Dholes/output/fastq_dl_%A_%a.err
 
@@ -14,7 +14,7 @@ echo "My SLURM_ARRAY_TASK_ID: ${SLURM_ARRAY_TASK_ID}"
 
 LIST="/scratch/ak308/Dholes/00_alignment/publishedfastq/fastq_list.tsv"
 
-NAME=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ${LIST} | cut -f1)
+NAME=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /scratch/ak308/Dholes/00_alignment/publishedfastqfastq_list.tsv | cut -f1)
 URL=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ${LIST} | cut -f2)
 
 wget ${URL}
