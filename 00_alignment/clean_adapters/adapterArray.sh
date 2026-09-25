0def1713a92fccaaed0d2c6e168fa11e8de1f585
Annamika Konkola <ak308@rice.edu>
2026-09-25 11:04:42 -0500

    Update adapterArray.sh

--- 00_alignment/clean_adapters/adapterArray.sh
+++ 00_alignment/clean_adapters/adapterArray.sh
@@ -11,12 +11,10 @@
  
 echo "My SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
  
-# Tab-separated, no header: <read1 file> <read2 file> <sample name>
-LIST=/scratch/YOUR_NETID/Dhole/publishedfastqs/fastqlist_pairs.txt
-DIR=/scratch/YOUR_NETID/Dhole/publishedfastqs
-OUT=/scratch/YOUR_NETID/Dhole/cleanedfastq
+LIST=/scratch/ak308/Dholes/00_alignment/publishedfastqs/fastqlist_pairs.txt
+DIR=/scratch/ak308/Dholes/00_alignment/publishedfastqs
+OUT=/scratch/ak308/Dholes/00_alignment/cleanedfastq
  
-# Pull line number $SLURM_ARRAY_TASK_ID from the list, then take column 1, 2, 3
 READ1=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ${LIST} | cut -f1)
 READ2=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ${LIST} | cut -f2)
 NAME=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ${LIST} | cut -f3)
@@ -25,8 +23,6 @@ echo "READ1: ${READ1}"
 echo "READ2: ${READ2}"
 echo "NAME:  ${NAME}"
  
-# Software path left unchanged: this is a shared install in lh106's folder.
-# If you get "Permission denied", install your own copy and point this at it.
 /scratch/lh106/software/adapterremoval-v3.0.1-linux-x86_64/adapterremoval3 \
     --in-file1 ${DIR}/${READ1} \
     --in-file2 ${DIR}/${READ2} \
